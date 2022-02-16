3b9be0f58b769b5870503f72ccf64463e4621955
Devon Wigle <devonwigle@gmail.com>
2022-02-16 11:36:29 -0600

    Build return elements

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Reservation = (props) => {
+const Reservation = ({id, name, date, time, number }) => {
   return (
-    <h2>Hello</h2>
+    <section id={id} key={id}>
+      <h3>{name}</h3>
+      <h3>Date: {date}</h3>
+      <h3>Time: {time}</h3>
+      <h3>Number in Party: {number}</h3>
+    </section>
   )
 }
 
