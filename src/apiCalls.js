c4eefd7a4a234d0b4f7f9e181f811bf4525a623c
Devon Wigle <devonwigle@gmail.com>
2022-02-16 12:17:06 -0600

    Write post fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,4 +8,23 @@ const getReservations = () => {
     })
 }
 
+const postReservation = (reservation, onSuccess) => {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reservation),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Something\'s gone wrong')
+      }
+      return response.json()
+    })
+    .then(() => onSuccess)
+    .catch(error => console.log(error))
+}
+
+
 export default getReservations
